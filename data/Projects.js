e2ccd63c5c83d099ffdb4f1501fed7f670340b9c
Alberto Alvarez <albertoalvarez@vinixcode.com>
2025-01-21 19:32:35 -0500

    Descripcion de proyectos (falta lo otro)

--- data/Projects.js
+++ data/Projects.js
@@ -1,40 +1,64 @@
 export const projects_es = [
   {
-    title: "E-commerce Platform",
+    title: "Wasapi",
     description:
-      "Plataforma de comercio electrónico con más de 100,000 usuarios activos.",
+      "Plataforma que centraliza y automatiza la comunicación vía WhatsApp, ofreciendo herramientas como chatbots dinámicos y con inteligencia artificial, campañas de mensajería, segmentación de audiencia y más. Facilita la atención al cliente, impulsa las ventas y simplifica las comunicaciones.",
     technologies: "React, Next.js, Tailwind CSS, MySQL, Node.js, Express",
     image:
       "https://images.unsplash.com/photo-1557821552-17105176677c?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
   },
   {
-    title: "CRM System",
+    title: "Wasapi - App Mobile",
     description:
-      "Sistema de gestión de clientes para una empresa de servicios de marketing masivo.",
+      "Aplicación móvil que centraliza y simplifica la comunicación vía WhatsApp. Permite visualizar y enviar mensajes, buscar y filtrar conversaciones, y gestionar chats. Ofrece soporte para mensajes multimedia y cuenta con modos claro y oscuro para una experiencia personalizada y accesible.",
     technologies: "Laravel, PHP, Vue.js, Bootstrap, Bootstrap Vue, jQuery",
     image:
       "https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
   },
   {
-    title: "Mobile App",
+    title: "CRM - STARS PLASTIC SURGERY",
     description:
-      "Aplicación móvil para la gestión de inventarios en una empresa de servicios de marketing masivo.",
+      "Una innovadora herramienta diseñada para gestionar de manera integral la comunicación entre una clínica y sus pacientes. Incluye funciones avanzadas como chat en tiempo real, gestión de contactos, administración de proveedores y comunicaciones integradas, ofreciendo una solución completa y eficiente.",
     technologies: "React Native, Expo, Firebase, Node.js, Express",
     image:
       "https://images.unsplash.com/photo-1551650975-87deedd944c3?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
   },
   {
-    title: "Web App",
+    title: "STARS PLASTIC SURGERY - App Mobile",
     description:
-      "Sistema de administración para una empresa de servicios de marketing masivo.",
+      "Desarrollé una aplicación móvil para gestionar la comunicación entre una clínica y sus pacientes. La app incluye funcionalidades como chat en tiempo real, gestión de citas, recordatorios y acceso a información médica relevante, brindando una solución eficiente para facilitar la interacción.",
     technologies: "Laravel, PHP, Vue.js",
     image:
       "https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&q=80&w=800",
   },
   {
-    title: "Web App",
+    title: "Match Fit - App Mobile",
     description:
-      "Sistema de administración para una empresa de servicios de marketing masivo.",
+      "Aplicación móvil que permite generar rutinas de entrenamiento mediante IA para jugadores de fútbol. Ofrece programas de entrenamiento adaptados a diferentes niveles, seguimiento de progreso, enfocados en mejorar la fuerza, velocidad y condición física de los jugadores, desde principiantes hasta avanzados.",
+    technologies: "Laravel, PHP, Vue.js",
+    image:
+      "https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&q=80&w=800",
+  },
+  {
+    title: "Metis Foundation",
+    description:
+      "Plataforma innovadora diseñada para gestionar y optimizar los gastos de los grants. Permite asignar investigadores, generar y descargar informes detallados, y acceder a estadísticas avanzadas para un monitoreo eficiente y transparente del manejo de los recursos en todo momento.",
+    technologies: "Laravel, PHP, Vue.js",
+    image:
+      "https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?auto=format&fit=crop&q=80&w=800&h=400",
+  },
+  {
+    title: "Master 2000 - Gestión Académica",
+    description:
+      "Software especializado en la gestión académica para instituciones educativas. Permite optimizar procesos administrativos y académicos mediante funcionalidades como la gestión de matrículas, control de notas y asistencia, comunicación entre docentes y estudiantes, y generación de reportes personalizados.",
+    technologies: "Laravel, PHP, Vue.js",
+    image:
+      "https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?auto=format&fit=crop&q=80&w=800&h=400",
+  },
+  {
+    title: "Dominus - Ingeniería Apropiada",
+    description:
+      "Plataforma diseñada para optimizar la operación de estaciones de servicio de combustible. Proporciona herramientas para el control de inventarios, gestión de ventas. Ofrece reportes detallados y acceso a información en tiempo real, mejorando la eficiencia y la toma de decisiones en las estaciones de servicio.",
     technologies: "Laravel, PHP, Vue.js",
     image:
       "https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?auto=format&fit=crop&q=80&w=800&h=400",
