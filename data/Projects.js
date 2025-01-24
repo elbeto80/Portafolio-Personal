f7de1e3164d25c13037141e28a48405b0a0da1e2
Alberto Alvarez <albertoalvarez@vinixcode.com>
2025-01-24 10:36:30 -0500

    Tecnologia en proyectos

--- data/Projects.js
+++ data/Projects.js
@@ -3,7 +3,8 @@ export const projects_es = [
     title: "Wasapi",
     description:
       "Plataforma que centraliza y automatiza la comunicación vía WhatsApp, ofreciendo herramientas como chatbots dinámicos y con inteligencia artificial, campañas de mensajería, segmentación de audiencia y más. Facilita la atención al cliente, impulsa las ventas y simplifica las comunicaciones.",
-    technologies: "React, Next.js, Tailwind CSS, MySQL, Node.js, Express",
+    technologies:
+      "PHP, Laravel, Vue.js, Nuxt.js, MySQL, MongoDB, AWS, Vuetify, Whatsapp API, Pusher",
     image:
       "https://images.unsplash.com/photo-1557821552-17105176677c?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
   },
@@ -11,7 +12,7 @@ export const projects_es = [
     title: "Wasapi - App Mobile",
     description:
       "Aplicación móvil que centraliza y simplifica la comunicación vía WhatsApp. Permite visualizar y enviar mensajes, buscar y filtrar conversaciones, y gestionar chats. Ofrece soporte para mensajes multimedia y cuenta con modos claro y oscuro para una experiencia personalizada y accesible.",
-    technologies: "Laravel, PHP, Vue.js, Bootstrap, Bootstrap Vue, jQuery",
+    technologies: "React, React Native, Expo, React Native Paper, Firebase",
     image:
       "https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
   },
@@ -19,7 +20,7 @@ export const projects_es = [
     title: "CRM - STARS PLASTIC SURGERY",
     description:
       "Una innovadora herramienta diseñada para gestionar de manera integral la comunicación entre una clínica y sus pacientes. Incluye funciones avanzadas como chat en tiempo real, gestión de contactos, administración de proveedores y comunicaciones integradas, ofreciendo una solución completa y eficiente.",
-    technologies: "React Native, Expo, Firebase, Node.js, Express",
+    technologies: "PHP, Laravel, Vue.js, Nuxt.js, MySQL, Vuetify",
     image:
       "https://images.unsplash.com/photo-1551650975-87deedd944c3?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
   },
@@ -27,7 +28,7 @@ export const projects_es = [
     title: "STARS PLASTIC SURGERY - App Mobile",
     description:
       "Desarrollé una aplicación móvil para gestionar la comunicación entre una clínica y sus pacientes. La app incluye funcionalidades como chat en tiempo real, gestión de citas, recordatorios y acceso a información médica relevante, brindando una solución eficiente para facilitar la interacción.",
-    technologies: "Laravel, PHP, Vue.js",
+    technologies: "React, React Native, Expo, React Native Paper, Firebase",
     image:
       "https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&q=80&w=800",
   },
@@ -35,7 +36,8 @@ export const projects_es = [
     title: "Match Fit - App Mobile",
     description:
       "Aplicación móvil que permite generar rutinas de entrenamiento mediante IA para jugadores de fútbol. Ofrece programas de entrenamiento adaptados a diferentes niveles, seguimiento de progreso, enfocados en mejorar la fuerza, velocidad y condición física de los jugadores, desde principiantes hasta avanzados.",
-    technologies: "Laravel, PHP, Vue.js",
+    technologies:
+      "React, React Native, Expo, React Native Paper, Firebase, Node.js, Express, AWS",
     image:
       "https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&q=80&w=800",
   },
@@ -43,7 +45,7 @@ export const projects_es = [
     title: "Metis Foundation",
     description:
       "Plataforma innovadora diseñada para gestionar y optimizar los gastos de los grants. Permite asignar investigadores, generar y descargar informes detallados, y acceder a estadísticas avanzadas para un monitoreo eficiente y transparente del manejo de los recursos en todo momento.",
-    technologies: "Laravel, PHP, Vue.js",
+    technologies: "PHP, Laravel, Vue.js, Nuxt.js, MySQL, Vuetify, Quickbooks",
     image:
       "https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?auto=format&fit=crop&q=80&w=800&h=400",
   },
@@ -51,7 +53,7 @@ export const projects_es = [
     title: "Master 2000 - Gestión Académica",
     description:
       "Software especializado en la gestión académica para instituciones educativas. Permite optimizar procesos administrativos y académicos mediante funcionalidades como la gestión de matrículas, control de notas y asistencia, comunicación entre docentes y estudiantes, y generación de reportes personalizados.",
-    technologies: "Laravel, PHP, Vue.js",
+    technologies: "PHP, Laravel, Vue.js, MySQL, SQL Server, Bootstrap, jQuery",
     image:
       "https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?auto=format&fit=crop&q=80&w=800&h=400",
   },
@@ -59,7 +61,8 @@ export const projects_es = [
     title: "Dominus - Ingeniería Apropiada",
     description:
       "Plataforma diseñada para optimizar la operación de estaciones de servicio de combustible. Proporciona herramientas para el control de inventarios, gestión de ventas. Ofrece reportes detallados y acceso a información en tiempo real, mejorando la eficiencia y la toma de decisiones en las estaciones de servicio.",
-    technologies: "Laravel, PHP, Vue.js",
+    technologies:
+      "PHP, Laravel, React, Vue.js, MySQL, SQL Server, Bootstrap, jQuery",
     image:
       "https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?auto=format&fit=crop&q=80&w=800&h=400",
   },
