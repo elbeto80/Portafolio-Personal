938a9303233c77e75bde75ba2ed9e10049719eeb
Alberto Alvarez <albertoalvarez@vinixcode.com>
2025-01-24 13:25:33 -0500

    Links e iconos en los proyectos

--- components/sections/Projects.tsx
+++ components/sections/Projects.tsx
@@ -4,9 +4,12 @@ import { motion } from "framer-motion";
 import { useTranslation } from "react-i18next";
 import { Card } from "@/components/ui/card";
 import Image from "next/image";
-import { projects_es, projects_en } from "@/data/Projects";
+import { projects_es, projects_en, projects_link } from "@/data/Projects";
+
+import { FaApple, FaGoogle, FaLink } from "react-icons/fa";
 
 interface ProjectsData {
+  name: string;
   title: string;
   description: string;
   technologies: string;
@@ -33,13 +36,36 @@ export default function Projects() {
         <div className="grid gap-6 md:grid-cols-3">
           {projects.map((project, i) => (
             <Card key={i} className="overflow-hidden">
-              <div className="relative h-48">
+              <div className="relative h-48 group">
                 <Image
                   src={project.image}
                   alt={project.title}
                   fill
-                  className="object-cover"
+                  className="object-cover transition-opacity group-hover:opacity-50"
                 />
+                <div className="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
+                  {projects_link[
+                    project.name as keyof typeof projects_link
+                  ].map((link, i) => (
+                    <a
+                      key={i}
+                      href={link.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="bg-white rounded-full p-1 ml-1"
+                    >
+                      <span className="text-black">
+                        {link.icon == "apple" ? (
+                          <FaApple className="w-6 h-6" />
+                        ) : link.icon == "google" ? (
+                          <FaGoogle className="w-6 h-6" />
+                        ) : (
+                          <FaLink className="w-6 h-6" />
+                        )}
+                      </span>
+                    </a>
+                  ))}
+                </div>
               </div>
               <div className="p-6">
                 <h3 className="text-xl font-semibold">{project.title}</h3>
--- data/Projects.js
+++ data/Projects.js
@@ -1,5 +1,6 @@
 export const projects_es = [
   {
+    name: "wasapi",
     title: "Wasapi",
     description:
       "Plataforma que centraliza y automatiza la comunicación vía WhatsApp, ofreciendo herramientas como chatbots dinámicos y con inteligencia artificial, campañas de mensajería, segmentación de audiencia y más. Facilita la atención al cliente, impulsa las ventas y simplifica las comunicaciones.",
@@ -9,6 +10,7 @@ export const projects_es = [
       "https://images.unsplash.com/photo-1557821552-17105176677c?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
   },
   {
+    name: "wasapi-app",
     title: "Wasapi - App Mobile",
     description:
       "Aplicación móvil que centraliza y simplifica la comunicación vía WhatsApp. Permite visualizar y enviar mensajes, buscar y filtrar conversaciones, y gestionar chats. Ofrece soporte para mensajes multimedia y cuenta con modos claro y oscuro para una experiencia personalizada y accesible.",
@@ -17,6 +19,7 @@ export const projects_es = [
       "https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
   },
   {
+    name: "stars-crm",
     title: "CRM - STARS PLASTIC SURGERY",
     description:
       "Una innovadora herramienta diseñada para gestionar de manera integral la comunicación entre una clínica y sus pacientes. Incluye funciones avanzadas como chat en tiempo real, gestión de contactos, administración de proveedores y comunicaciones integradas, ofreciendo una solución completa y eficiente.",
@@ -25,6 +28,7 @@ export const projects_es = [
       "https://images.unsplash.com/photo-1551650975-87deedd944c3?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
   },
   {
+    name: "stars-app",
     title: "STARS PLASTIC SURGERY - App Mobile",
     description:
       "Desarrollé una aplicación móvil para gestionar la comunicación entre una clínica y sus pacientes. La app incluye funcionalidades como chat en tiempo real, gestión de citas, recordatorios y acceso a información médica relevante, brindando una solución eficiente para facilitar la interacción.",
@@ -33,6 +37,7 @@ export const projects_es = [
       "https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&q=80&w=800",
   },
   {
+    name: "match-fit-app",
     title: "Match Fit - App Mobile",
     description:
       "Aplicación móvil que permite generar rutinas de entrenamiento mediante IA para jugadores de fútbol. Ofrece programas de entrenamiento adaptados a diferentes niveles, seguimiento de progreso, enfocados en mejorar la fuerza, velocidad y condición física de los jugadores, desde principiantes hasta avanzados.",
@@ -42,6 +47,7 @@ export const projects_es = [
       "https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&q=80&w=800",
   },
   {
+    name: "metis-foundation",
     title: "Metis Foundation",
     description:
       "Plataforma innovadora diseñada para gestionar y optimizar los gastos de los grants. Permite asignar investigadores, generar y descargar informes detallados, y acceder a estadísticas avanzadas para un monitoreo eficiente y transparente del manejo de los recursos en todo momento.",
@@ -50,6 +56,7 @@ export const projects_es = [
       "https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?auto=format&fit=crop&q=80&w=800&h=400",
   },
   {
+    name: "master-2000",
     title: "Master 2000 - Gestión Académica",
     description:
       "Software especializado en la gestión académica para instituciones educativas. Permite optimizar procesos administrativos y académicos mediante funcionalidades como la gestión de matrículas, control de notas y asistencia, comunicación entre docentes y estudiantes, y generación de reportes personalizados.",
@@ -58,6 +65,7 @@ export const projects_es = [
       "https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?auto=format&fit=crop&q=80&w=800&h=400",
   },
   {
+    name: "dominus-ingenieria",
     title: "Dominus - Ingeniería Apropiada",
     description:
       "Plataforma diseñada para optimizar la operación de estaciones de servicio de combustible. Proporciona herramientas para el control de inventarios, gestión de ventas. Ofrece reportes detallados y acceso a información en tiempo real, mejorando la eficiencia y la toma de decisiones en las estaciones de servicio.",
@@ -70,10 +78,137 @@ export const projects_es = [
 
 export const projects_en = [
   {
-    title: "E-commerce Platform",
-    description: "E-commerce platform with more than 100,000 active users.",
-    technologies: "React, Next.js, Tailwind CSS, MySQL, Node.js, Express",
+    name: "wasapi",
+    title: "Wasapi",
+    description:
+      "Platform that centralizes and automates communication via WhatsApp, offering tools like dynamic and AI-powered chatbots, messaging campaigns, audience segmentation, and more. It facilitates customer service, boosts sales, and simplifies communications.",
+    technologies:
+      "PHP, Laravel, Vue.js, Nuxt.js, MySQL, MongoDB, AWS, Vuetify, Whatsapp API, Pusher",
     image:
       "https://images.unsplash.com/photo-1557821552-17105176677c?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
   },
+  {
+    name: "wasapi-app",
+    title: "Wasapi - App Mobile",
+    description:
+      "Mobile application that centralizes and simplifies communication via WhatsApp. It allows users to view and send messages, search and filter conversations, and manage chats. It supports multimedia messages and includes light and dark modes for a personalized and accessible experience.",
+    technologies: "React, React Native, Expo, React Native Paper, Firebase",
+    image:
+      "https://images.unsplash.com/photo-1460925895917-afdab827c52f?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    name: "stars-crm",
+    title: "CRM - STARS PLASTIC SURGERY",
+    description:
+      "An innovative tool designed to comprehensively manage communication between a clinic and its patients. It includes advanced features like real-time chat, contact management, supplier administration, and integrated communications, offering a complete and efficient solution.",
+    technologies: "PHP, Laravel, Vue.js, Nuxt.js, MySQL, Vuetify",
+    image:
+      "https://images.unsplash.com/photo-1551650975-87deedd944c3?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60",
+  },
+  {
+    name: "stars-app",
+    title: "STARS PLASTIC SURGERY - App Mobile",
+    description:
+      "I developed a mobile application to manage communication between a clinic and its patients. The app includes features like real-time chat, appointment management, reminders, and access to relevant medical information, providing an efficient solution to facilitate interaction.",
+    technologies: "React, React Native, Expo, React Native Paper, Firebase",
+    image:
+      "https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&q=80&w=800",
+  },
+  {
+    name: "match-fit-app",
+    title: "Match Fit - App Mobile",
+    description:
+      "Mobile application that generates training routines through AI for soccer players. It offers training programs tailored to different levels, progress tracking, and focuses on improving strength, speed, and physical condition for players ranging from beginners to advanced.",
+    technologies:
+      "React, React Native, Expo, React Native Paper, Firebase, Node.js, Express, AWS",
+    image:
+      "https://images.unsplash.com/photo-1551288049-bebda4e38f71?auto=format&fit=crop&q=80&w=800",
+  },
+  {
+    name: "metis-foundation",
+    title: "Metis Foundation",
+    description:
+      "Innovative platform designed to manage and optimize grant expenses. It allows assigning researchers, generating and downloading detailed reports, and accessing advanced statistics for efficient and transparent resource management at all times.",
+    technologies: "PHP, Laravel, Vue.js, Nuxt.js, MySQL, Vuetify, Quickbooks",
+    image:
+      "https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?auto=format&fit=crop&q=80&w=800&h=400",
+  },
+  {
+    name: "master-2000",
+    title: "Master 2000 - Gestión Académica",
+    description:
+      "Software specialized in academic management for educational institutions. It optimizes administrative and academic processes through features like enrollment management, grade and attendance tracking, teacher-student communication, and personalized report generation.",
+    technologies: "PHP, Laravel, Vue.js, MySQL, SQL Server, Bootstrap, jQuery",
+    image:
+      "https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?auto=format&fit=crop&q=80&w=800&h=400",
+  },
+  {
+    name: "dominus-ingenieria",
+    title: "Dominus - Ingeniería Apropiada",
+    description:
+      "Platform designed to optimize the operation of fuel service stations. It provides tools for inventory control, sales management, and offers detailed reports and real-time information access, improving efficiency and decision-making in service stations.",
+    technologies:
+      "PHP, Laravel, React, Vue.js, MySQL, SQL Server, Bootstrap, jQuery",
+    image:
+      "https://images.unsplash.com/photo-1512941937669-90a1b58e7e9c?auto=format&fit=crop&q=80&w=800&h=400",
+  },
 ];
+
+export const projects_link = {
+  wasapi: [
+    {
+      link: "https://wasapi.io",
+      icon: "web",
+    },
+  ],
+  "wasapi-app": [
+    {
+      link: "https://play.google.com/store/apps/details?id=com.vinixcode.Wasapi",
+      icon: "google",
+    },
+    {
+      link: "https://apps.apple.com/us/app/wasapi/id6443781383",
+      icon: "apple",
+    },
+  ],
+  "stars-crm": [
+    {
+      link: "https://customers.starsplastics.com/",
+      icon: "web",
+    },
+  ],
+  "stars-app": [
+    {
+      link: "https://apps.apple.com/us/app/stars-gateway/id1572540209",
+      icon: "apple",
+    },
+    {
+      link: "https://play.google.com/store/apps/details?id=com.vinixcode.STARS_Gateway",
+      icon: "google",
+    },
+  ],
+  "match-fit-app": [
+    {
+      link: "https://apps.apple.com/us/app/match-fit/id6464080703",
+      icon: "apple",
+    },
+  ],
+  "metis-foundation": [
+    {
+      link: "https://grants.metisfoundationusa.org/",
+      icon: "web",
+    },
+  ],
+  "master-2000": [
+    {
+      link: "https://login.master2000.net/ingreso/index.php?A=Global&",
+      icon: "web",
+    },
+  ],
+  "dominus-ingenieria": [
+    {
+      link: "https://dominus.iapropiada.com/",
+      icon: "web",
+    },
+  ],
+};
